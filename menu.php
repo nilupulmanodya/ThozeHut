d6c7133167cc827954be811b37f258a1a3efb8f6
madushankamadu <madushankaforgit@gmail.com>
2021-07-18 07:37:19 +0530

    add to cart setled

--- menu.php
+++ menu.php
@@ -1,7 +1,30 @@
+
+	<?php 
+
+
+	require "php/connect.php";
+
+	if (isset($_POST['cart'])) {
+		$user_name = $_SESSION['username'];
+		$item_id = $_POST['item_id'];
+
+		if (mysqli_query($conn,"INSERT INTO cart (user_name,item_id) VALUES ('$user_name','$item_id')",)) {
+			header("Location:menu.php");
+			exit;
+			echo " data added ";
+		}else{
+			echo "faild to insert record";
+		}
+	}
+	?>
+
+
+
 <!DOCTYPE html>
 <html>
 	<head>
 
+		
 		<link rel="stylesheet" href="css/menu.css">
 		<link rel="stylesheet" href="css/style.css">
 
@@ -36,14 +59,14 @@ include_once 'includes/navbar.php';
 
 		<!--session1 starts here-->
 		<div class="container show" id="tabcontent-1">
-			<div class="board">
+			<div class="mboard">
 				<div class="row">
 				
 					<?php 
 	
 					require "php/connect.php";
 	
-	            	$sql1 = "SELECT * FROM items WHERE category='Food'";
+	            	$sql1 = "SELECT * FROM items WHERE category='foods'";
 	            	$result1 = mysqli_query($conn, $sql1);
 	            
 	            	if (mysqli_num_rows($result1) > 0) {
@@ -56,18 +79,24 @@ include_once 'includes/navbar.php';
 	            
 	            
 					<div class="column">	
-						<div class="card">
-  							<img src="images/food/<?php echo $row1["image_src"]; ?>" alt="Denim Jeans" style="width:100%; background-color: transparent;">
+						<form action="" method="post" class="card" id="add_to_cart">
+  							<img src="images/<?php echo $row1["image_src"]; ?>" alt="food" style="width:100%; background-color: transparent;">
   							<div class="detail">
   								<div class="name">
-  									<h1 style="margin-bottom:0px; margin-top: 15px; "><?php echo $row1["item_name"]; ?></h1>	
+  							 		<h1 style="margin-bottom:0px; margin-top: 15px; "><?php echo $row1["item_name"]; ?></h1>	
   								</div>
   								<div class="price" >
   									<p style="margin-bottom:0px; padding: 0 20%px;float: right;" ><?php echo $row1["price"] ?></p>
   								</div>
   							</div>
-  							<p><button ><img style="background-color: transparent; padding-right: 5%; width: 15px;" src="images/assets/cart.png">Add to Cart</button></p>
-						</div>
+	
+							<input type="hidden" name="item_id" value="<?php echo $row1["item_id"]; ?>" />
+
+
+  							<p><button value="Submit" name="cart" value="add" type="submit"><img style="background-color: transparent; padding-right: 5%; width: 15px;" src="images/assets/cart.png">Add to Cart</button></p>
+       						 	 
+    						
+						</form>
 					</div>	              
 	            <?php
 	                  if ($i%3 == 0) {
@@ -89,14 +118,14 @@ include_once 'includes/navbar.php';
 
 		<!--session2 starts here-->
 		<div class="container hide" id="tabcontent-2">
-			<div class="board">
+			<div class="mboard">
 				<div class="row">
 				
 					<?php 
 	
 					
 	
-	            	$sql2 = "SELECT * FROM items WHERE category='Drink'";
+	            	$sql2 = "SELECT * FROM items WHERE category='drinks'";
 	            	$result2 = mysqli_query($conn, $sql2);
 	            
 	            	if (mysqli_num_rows($result2) > 0) {
@@ -107,8 +136,8 @@ include_once 'includes/navbar.php';
 	            
 	            
 					<div class="column">	
-						<div class="card">
-  							<img src="images/food/<?php echo $row2["image_src"]; ?>" alt="Denim Jeans" style="width:100%; background-color: transparent;">
+						<form action="" method="post" class="card" id="add_to_cart">
+  							<img src="images/<?php echo $row2["image_src"]; ?>" alt="food" style="width:100%; background-color: transparent;">
   							<div class="detail">
   								<div class="name">
   									<h1 style="margin-bottom:0px; margin-top: 15px; "><?php echo $row2["item_name"]; ?></h1>	
@@ -117,8 +146,11 @@ include_once 'includes/navbar.php';
   									<p style="margin-bottom:0px; padding: 0 20%px;float: right;" ><?php echo $row2["price"] ?></p>
   								</div>
   							</div>
-  							<p><button ><img style="background-color: transparent; padding-right: 5%; width: 15px;" src="images/assets/cart.png">Add to Cart</button></p>
-						</div>
+
+  							<input type="hidden" name="item_id" value="<?php echo $row2["item_id"]; ?>" />
+
+  							<p><button value="Submit" name="cart" value="add" type="submit"><img style="background-color: transparent; padding-right: 5%; width: 15px;" src="images/assets/cart.png">Add to Cart</button></p>
+						</form>
 					</div>	              
 	            <?php
 	                  if ($j%3 == 0) {
@@ -140,14 +172,14 @@ include_once 'includes/navbar.php';
 
 		<!--session starts here-->
 		<div class="container hide" id="tabcontent-3">
-			<div class="board">
+			<div class="mboard">
 				<div class="row">
 				
 					<?php 
 	
 					
 	
-	            	$sql3 = "SELECT * FROM items WHERE category='Desert'";
+	            	$sql3 = "SELECT * FROM items WHERE category='desserts'";
 	            	$result3 = mysqli_query($conn, $sql3);
 	            
 	            	if (mysqli_num_rows($result3) > 0) {
@@ -158,8 +190,8 @@ include_once 'includes/navbar.php';
 	            
 	            
 					<div class="column">	
-						<div class="card">
-  							<img src="images/food/<?php echo $row3["image_src"]; ?>" alt="Denim Jeans" style="width:100%; background-color: transparent;">
+						<form action="" method="post" class="card" id="add_to_cart">
+  							<img src="./images/<?php echo $row3["image_src"]; ?>" alt="food" style="width:100%; background-color: transparent;">
   							<div class="detail">
   								<div class="name">
   									<h1 style="margin-bottom:0px; margin-top: 15px; "><?php echo $row3["item_name"]; ?></h1>	
@@ -168,8 +200,11 @@ include_once 'includes/navbar.php';
   									<p style="margin-bottom:0px; padding: 0 20%px;float: right;" ><?php echo $row3["price"] ?></p>
   								</div>
   							</div>
-  							<p><button ><img style="background-color: transparent; padding-right: 5%; width: 15px;" src="images/assets/cart.png">Add to Cart</button></p>
-						</div>
+
+  							<input type="hidden" name="item_id" value="<?php echo $row3["item_id"]; ?>" />
+
+  							<p><button value="Submit" name="cart" value="add" type="submit"><img style="background-color: transparent; padding-right: 5%; width: 15px;" src="images/assets/cart.png">Add to Cart</button></p>
+						</form>
 					</div>	              
 	            <?php
 	                  if ($k%3 == 0) {
@@ -190,8 +225,12 @@ include_once 'includes/navbar.php';
      </div>
 
 	 <?php
-include_once 'includes/footer.php';
-?>
+	include_once 'includes/footer.php';
+	?>
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 
 		<script>
 			function showTab(tabNumber) {
@@ -205,8 +244,6 @@ include_once 'includes/footer.php';
 
 }
 
-
 		</script>
-
 	</body>
 </html>
